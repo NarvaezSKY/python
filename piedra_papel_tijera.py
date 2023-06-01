511521b32ebab2ca266e2d6b3b26a12eb33d993c
cristian <crissnar12xd@gmail.com>
2023-06-01 11:21:15 -0500

    piedra papel o tijera

---
+++ piedra_papel_tijera.py
@@ -0,0 +1,15 @@
+maquina="piedra"
+eleccion=input("Ingresa tu eleccion: ")
+
+if eleccion=="tijera":
+    print("gana maquina. Eleccion maquina:", maquina) 
+            
+elif eleccion==maquina: 
+    print("empate. Eleccion maquina:", maquina)   
+
+elif eleccion=="papel":
+    print("gana usuario. eleccion maquina:", maquina)
+    
+else:
+    ("opción invalida")
+        
